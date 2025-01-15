59cff8ab007e61a16ffdbceb4e9487d72c6e6bb2
Todd Nobles <tonobles@gmail.com>
2025-01-14 16:52:25 -0800

    adding in the better parenthood variable

--- R checks/1_data_intake.R
+++ R checks/1_data_intake.R
@@ -18,7 +18,7 @@ for (x in ds) {
                       'TDEBT_HOME', 'THDEBT_HOME', 'TDEBT_BUS', 'TTHR401VAL', 'TIRAKEOVAL', 
                       'TVAL_AST', 'THVAL_AST', 'TNETWORTH', 'THNETWORTH', 'TVAL_HOME', 
                       'THVAL_HOME', 'TEQ_HOME', 'THEQ_HOME', 'TEQ_BUS', 'TPTOTINC', 
-                      'TPEARN', 'ENJFLAG')
+                      'TPEARN', 'ENJFLAG', 'EPAR_SCRNR')
   
   # Match column names after coercing all to same case
   selected_columns <- cols[tolower(cols) %in% tolower(target_columns)]
@@ -43,7 +43,7 @@ for (x in ds) {
   
   
   # handling annoying case inconsistencies in their coding.
-  target_columns <- c('SSUID', 'PNUM', 'MONTHCODE', 'SPANEL', 'SWAVE')
+  target_columns <- c('SSUID', 'PNUM', 'MONTHCODE', 'SPANEL', 'SWAVE', "TAGE")
   
   # Match column names after coercing all to same case
   selected_columns <- cols[tolower(cols) %in% tolower(target_columns)]
